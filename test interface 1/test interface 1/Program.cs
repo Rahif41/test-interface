5d6b0f65d7e3c3fe49fcf5d9fbf0dc1ef537783e
Rahif hasan <18-36365-1@student.aiub.edu>
2020-11-30 15:54:18 +0600

    test interface

---
+++ test interface 1/test interface 1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator|");
+            Console.WriteLine();
+            Console.WriteLine("Basic Calculator Interface");
+            Console.WriteLine();
+            BasicCalc b = new BasicCalc();
+            Console.WriteLine("Summation Function Result");
+
+            b.sum(20, 10);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Subtraction Function Result");
+           
+            b.sub(20, 10);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Multiplication Function Result");
+           
+            b.multiplication(20, 10);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine("Division Function Result");
+          
+            b.division(20, 10);
+            b.printStates();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Scientific Calculator Interface ");
+            Console.WriteLine();
+            SciCal s = new SciCal();
+            Console.WriteLine();
+            Console.WriteLine("Division Function Result");
+            Console.WriteLine();
+            s.division(20, 10);
+            Console.WriteLine("Factorial Function Result");
+            Console.WriteLine();
+            s.factorial(10);
+            Console.WriteLine();
+            Console.WriteLine("X to Y Function Result");
+            Console.WriteLine();
+            s.XtoY(100, 500);
+
+        }
+    }
+}
---
+++ test interface 1/test interface 1/basiccalculatorinterface.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_interface_1
+{
+    class basiccalculatorinterface
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+    }
+    class Basiccalc : Basiccalculatorinterface
+    {
+        int result;
+        public int sum(int number1, int number2)
+        {
+            Console.WriteLine("Summation : " + number1 + " With : " + number2);
+            result = number1 + number2;
+            return result;
+        }
+        public int sub(int number1, int number2)
+        {
+            Console.WriteLine("Subtraction : " + number1 + " By : " + number2);
+            result = number1 - number2;
+            return result;
+        }
+        public int multiplication(int number1, int number2)
+        {
+            Console.WriteLine("Multiplication : " + number1 + " With : " + number2);
+            result = number1 * number2;
+            return result;
+        }
+        public int division(int number1, int number2)
+        {
+            Console.WriteLine("Divident : " + number1 + " & Divisor : " + number2);
+            result = number1 / number2;
+            return result;
+        }
+        public void printStates()
+        {
+            Console.WriteLine("Result : " + result);
+        }
+    }
+}
